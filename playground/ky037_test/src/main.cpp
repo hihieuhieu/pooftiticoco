c3e705a6a5fe0179449cbfd053cb507a848f63b6
Hieu Philip Hoang <hieuphiliphoang@yahoo.de>
2025-01-03 22:09:31 +0100

    sampling frequency measurement

--- playground/ky037_test/src/main.cpp
+++ playground/ky037_test/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include "time.h"
+#include <Async_Operations.h> //! think of necessity of async stuff
 
 int analogInputPin = A0;
 int digitalInputPin = 2;
@@ -6,17 +8,20 @@ int digitalInputPin = 2;
 void setup() {
   pinMode(analogInputPin, INPUT);
   pinMode(digitalInputPin, INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Initialized");
 }
 
 void loop() {
   float measured_value;
 
+  unsigned long start_time = micros();
+
   measured_value = analogRead(analogInputPin) * (5.0/1023.0);
 
-  // Serial.println('A');
-
-  Serial.println(measured_value, 4);
+  Serial.print(measured_value, 4);
+  
+  Serial.print("   ");
+  Serial.println((1/float(micros() - start_time))*1e6,10);
   
 }
