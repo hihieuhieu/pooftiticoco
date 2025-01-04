1f983bfdc5f113f5c4c42c9b07782f26cfc8c03e
Hieu Philip Hoang <hieuphiliphoang@yahoo.de>
2025-01-04 01:12:43 +0100

    included median filter to avoid that weird spikes

--- playground/visualize_data.py
+++ playground/visualize_data.py
@@ -3,34 +3,43 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from collections import deque
 import numpy as np
+from scipy.signal import medfilt
 
 # Configuration
 port = '/dev/ttyACM0'
-baud_rate = 9600
+baud_rate = 115200
 max_data_points = 1000
 
 ser = serial.Serial(port, baud_rate)
 
-data = deque([0] * max_data_points, maxlen=max_data_points)
+sensor_data = deque([0] * max_data_points, maxlen=max_data_points)
+instantaneous_sampling_frequency = deque([0] * max_data_points, maxlen=max_data_points)
 
 # Create figure
-fig, ax = plt.subplots()
-line, = ax.plot(data)
-ax.set_ylim(0, 5) 
+fig, ax1 = plt.subplots()
+sensor_data_line, = ax1.plot(sensor_data)
+ax1.set_ylim(0, 5) 
 
 def update(frame):
     try:
-        value = float(ser.readline().decode('utf-8').strip())
-        data.append(value)
-        line.set_ydata(data)
+        values = (ser.readline().decode('utf-8').strip()).split('~')
 
-        line.set_ydata(data)
-        ax.set_ylim(min(data) - 0.1, max(data) + 0.1)
+        if values:
+            sensor_data.append(float(values[0]))
+            instantaneous_sampling_frequency.append(float(values[1]))
+
+        sensor_data_filtered = medfilt(sensor_data, kernel_size=5)
+
+        sensor_data_line.set_ydata(sensor_data_filtered)
+
+        print(f'min: {min(sensor_data)}, max: {max(sensor_data)}')
+
+        ax1.set_ylim(min(sensor_data_filtered) - 0.1, max(sensor_data_filtered) + 0.1)
 
     except Exception as e:
         print(f"Error: {e}")
 
-    return line,
+    return sensor_data_line,
 
 # interval in ms
 ani = animation.FuncAnimation(fig, update, interval=1, blit=True, cache_frame_data=False)
